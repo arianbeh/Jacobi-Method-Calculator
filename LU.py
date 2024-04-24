519102f7a7187a6df05008849d9ff2f46b6fb1ee
tonyt6a <trui2005@gmail.com>
2024-04-23 20:55:24 -0500

    Changed LU, solved 4-7
    Changed LU to be more in line with what we learned in class and to account for the band structure, added create b function for question 6

--- LU.py
+++ LU.py
@@ -1,19 +1,39 @@
 import numpy as np
-import testing
+import Onk
 import time
+import matplotlib.pyplot as plt
 
 def lu_factorization(A):
-    n = len(A)
-    L = [[0.0] * n for _ in range(n)]
-    U = [[0.0] * n for _ in range(n)]
+    n = A.shape[0]
+    U = A.copy()
+    U = U.astype(float)
+    L = np.zeros((n, n))
+    N = int(np.sqrt(n))
+    # Create identity
+    for i in range(n):
+        L[i, i] = 1
     operations = 0
+    for i in range(n - 1):
+        for j in range(i + 1, i + N + 1):
+            if j < n:
+                factor = U[j, i] / U[i, i]
+                L[j, i] = factor
+                U[j] -= factor * U[i] 
+                operations += 1       
+    return L, U, operations
 
+
+def better_lu_factorization(A):
+    n = len(A)
+    L = np.zeros((n, n))
+    U = np.zeros((n, n))
+    operations = 0
     for i in range(n):
         # Upper Triangular Matrix
         for k in range(i, n):
             sum_ = sum(L[i][j] * U[j][k] for j in range(i))
             U[i][k] = A[i][k] - sum_
-            operations += 1
+            operations += j
 
         # Lower Triangular Matrix
         for k in range(i, n):
@@ -22,7 +42,7 @@ def lu_factorization(A):
             else:
                 sum_ = sum(L[k][j] * U[j][i] for j in range(i))
                 L[k][i] = (A[k][i] - sum_) / U[i][i]
-            operations += 1
+                operations += j
 
     return L, U, operations
 
@@ -33,11 +53,18 @@ def count_nonzero_entries(matrix):
             if element != 0:
                 count += 1
     return count
+def create_b(N):
+    b = np.zeros((N * N))
+    N = np.floor((N * N) / 2).astype(int)
+    b[N] = 1
+    return b
 
 # Main program
-for N in range(1, 8):
+N_list = []
+iterations_list = []
+for N in range(1, 16):
     # Generate ∆N matrix
-    delta_N = [[0] * (N*N) for _ in range(N*N)]
+    delta_N = np.zeros((N*N, N*N), dtype=float)
     for i in range(N*N):
         for j in range(N*N):
             p, q = divmod(i, N)
@@ -46,32 +73,52 @@ for N in range(1, 8):
                 delta_N[i][j] = 1
             if i == j:
                 delta_N[i][j] = -4
-    print(np.matrix(delta_N))
+    print(f"For N = {N}:")
+    # print(np.matrix(delta_N))
 
     # Perform LU-factorization
-    L, U, operations = lu_factorization(delta_N)
+    # A_1 = np.copy(delta_N).astype(np.float64)
+    # L, U, operations = lu_factorization(A_1)
+    # result = L @ U
+    # result = np.round(result).astype(int)
+    # print("LU worked? ", np.array_equal(result, delta_N))
+    # print("LU = ")
+    # print(result)
 
     # Count non-zero entries in matrices L and U
-    nonzero_L = count_nonzero_entries(L)
-    nonzero_U = count_nonzero_entries(U)
-    b = np.random.randint(-10, 10, size=(N,))
-    epsilon = 1 * 10**-14
+    # nonzero_L = count_nonzero_entries(L)
+    # nonzero_U = count_nonzero_entries(U)
+    # print("L: ")
+    # print(np.matrix(L))
+    # print("U: ")
+    # np.set_printoptions(precision=4, suppress=True)
+    # print(np.matrix(U))
+    b = np.random.randint(-10, 10, size=(N * N,))
+    # b = create_b(N)
+    epsilon = 10**-12
 
-    print("A:", delta_N)
-    print("b:", b)
-    print("size of A: ", N * N)
+    # print("A:", delta_N)
+    # print("b:", b)
+    print("size of A: ", N ** 4)
     start_time = time.time()
     expected = np.linalg.solve(delta_N, b)
     end_time = time.time()
     print("Elapsed time for numpy: ", end_time - start_time)
     start_time = time.time()
-    actual, iterations = Onk.jacobi_method(delta_N, b, epsilon)
+    actual, iterations, operations = Onk.jacobi_method(delta_N, b, epsilon)
     end_time = time.time()
     print("Elapsed time for Jacobi: ", end_time - start_time)
+    print("Number of operations for Jacobi: ", operations)
+    print("Number of operations per iteration: ", operations/ iterations)
+    print("Number of iterations: ", iterations)
+    N_list.append(N)
+    iterations_list.append(iterations)
+
 
     # Print results
-    print(f"For N = {N}:")
-    print("Number  of Operations: ", operations)
-    print("Number of operations in terms of N: ", operations/N, "N")
-    print(f"Non-zero entries in L: {nonzero_L}")
-    print(f"Non-zero entries in U: {nonzero_U}")
+    # print("Number  of Operations: ", operations)
+    # print("Number of operations in terms of N: ", operations/N, "N")
+    # print(f"Non-zero entries in L: {nonzero_L}")
+    # print(f"Non-zero entries in U: {nonzero_U}")
+print(N_list)
+print(iterations_list)
--- Onk.py
+++ Onk.py
@@ -7,6 +7,7 @@ def jacobi_method(A, b, epsilon):
     print("Size of new A:", len(entries) * 2)
     x = np.zeros(n, dtype=np.float64)
     iteration_count = 0 
+    operations = 0
     while True:
         x_new = np.zeros(n, dtype=np.float64)
         x_count = 0
@@ -16,16 +17,20 @@ def jacobi_method(A, b, epsilon):
             while jump[i] != -1:
                 sum += -(entries[i] * x[jump[i]])
                 i += 1
+                operations += 1
             x_new[x_count] = (sum + b[x_count]) / entries[i]
+            operations += 1
             x_count += 1
             i += 1
         # print("Current iteration solved: ", x_new)
         result = x_new - x
+        operations += 1
         norm = np.float64(0)
         for i in result:
             norm += i ** 2
-        if norm < epsilon or iteration_count >= 500:
-            return x_new, iteration_count
+            operations += 1
+        if norm < epsilon:
+            return x_new, iteration_count, operations
         x = x_new
         iteration_count += 1
 def sparse_matrix(A, n):
@@ -70,24 +75,24 @@ def generate_diag_dom(n):
         if (np.linalg.det(sparse_matrix) != 0):
             return sparse_matrix
 
-n = 160
-A = generate_diag_dom(n)
-b = np.random.randint(-10, 10, size=(n,))
-epsilon = 1 * 10**-14
+# n = 160
+# A = generate_diag_dom(n)
+# b = np.random.randint(-10, 10, size=(n,))
+# epsilon = 1 * 10**-14
 
-print("A:", A)
-print("b:", b)
-print("Target size: ", max_nonzeroes_in_row(A) * n)
-print("size of A: ", n * n)
-start_time = time.time()
-expected = np.linalg.solve(A, b)
-end_time = time.time()
-print("Elapsed time for numpy: ", end_time - start_time)
-start_time = time.time()
-actual, iterations = jacobi_method(A, b, epsilon)
-end_time = time.time()
-print("Elapsed time for Jacobi: ", end_time - start_time)
+# print("A:", A)
+# print("b:", b)
+# print("Target size: ", max_nonzeroes_in_row(A) * n)
+# print("size of A: ", n * n)
+# start_time = time.time()
+# expected = np.linalg.solve(A, b)
+# end_time = time.time()
+# print("Elapsed time for numpy: ", end_time - start_time)
+# start_time = time.time()
+# actual, iterations = jacobi_method(A, b, epsilon)
+# end_time = time.time()
+# print("Elapsed time for Jacobi: ", end_time - start_time)
 
-print("Iterations: ", iterations)
-print("Actual:", actual)
-print("Expected:", expected)
+# print("Iterations: ", iterations)
+# print("Actual:", actual)
+# print("Expected:", expected)
